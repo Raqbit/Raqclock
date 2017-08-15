89f43b54ba9da58d75e486a1ef8a878f27c94673
Raqbit <raqbit@outlook.com>
2017-08-15 16:02:38 +0200

    Removed references to the seed

--- src/electron/electron-app.ts
+++ src/electron/electron-app.ts
@@ -10,6 +10,64 @@ let wpi;
 
 const pressed = {};
 
+const mainWindowSettings: Electron.BrowserWindowConstructorOptions = {
+    width: 320,
+    height: 240,
+    fullscreen: true,
+    frame: false,
+    resizable: false,
+    kiosk: true,
+};
+
+function initMainListener() {
+    ipcMain.on('ELECTRON_BRIDGE_HOST', (event, msg) => {
+
+    });
+}
+
+function createWindow() {
+    if (debugMode) {
+        mainWindowSettings.frame = true;
+        mainWindowSettings.fullscreen = false;
+    }
+    applicationRef = new BrowserWindow(mainWindowSettings);
+    applicationRef.loadURL(`file:///${__dirname}/index.html`);
+    if (debugMode) {
+        // Open the DevTools.
+        applicationRef.webContents.openDevTools({ mode: 'detach' });
+    }
+    applicationRef.on('closed', () => {
+        // Dereference the window object, usually you would store windows
+        // in an array if your app supports multi windows, this is the time
+        // when you should delete the corresponding element.
+        applicationRef = null;
+    });
+
+    initMainListener();
+
+    client.create(applicationRef);
+
+    setupGPIO();
+}
+
+
+app.on('ready', createWindow);
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+        // TODO perhaps hook this and wait for message bus before quitting?
+        app.quit();
+    }
+});
+
+app.on('activate', () => {
+    // On macOS it's common to re-create a window in the app when the
+    // dock icon is clicked and there are no other windows open.
+    if (applicationRef === null) {
+        createWindow();
+    }
+});
+
 function setupGPIO() {
     if (!debugMode) {
         wpi = require('wiring-pi');
@@ -62,63 +120,7 @@ function buttonDown(buttonNum) {
 
 function buttonUp(buttonNum) {
     if (!pressed[buttonNum]) { return; }
-    const timestamp = new Date().getTime();
     const duration = (new Date().getTime() - pressed[buttonNum]);
 
-    console.log('Button UP: ' + buttonNum + ': ' + duration);
+    ipcMain.emit('ELECTRON_BRIDGE_CLIENT', { buttonNum: buttonNum, duration: duration });
 }
-
-const mainWindowSettings: Electron.BrowserWindowConstructorOptions = {
-    width: 320,
-    height: 240,
-    frame: false,
-    resizable: false
-};
-
-function initMainListener() {
-    ipcMain.on('ELECTRON_BRIDGE_HOST', (event, msg) => {
-
-    });
-}
-
-function createWindow() {
-    if (debugMode) {
-        // mainWindowSettings.frame = true;
-    }
-    applicationRef = new BrowserWindow(mainWindowSettings);
-    applicationRef.loadURL(`file:///${__dirname}/index.html`);
-    if (debugMode) {
-        // Open the DevTools.
-        applicationRef.webContents.openDevTools({ mode: 'detach' });
-    }
-    applicationRef.on('closed', () => {
-        // Dereference the window object, usually you would store windows
-        // in an array if your app supports multi windows, this is the time
-        // when you should delete the corresponding element.
-        applicationRef = null;
-    });
-
-    initMainListener();
-
-    client.create(applicationRef);
-
-    setupGPIO();
-}
-
-
-app.on('ready', createWindow);
-
-app.on('window-all-closed', () => {
-    if (process.platform !== 'darwin') {
-        // TODO perhaps hook this and wait for message bus before quitting?
-        app.quit();
-    }
-});
-
-app.on('activate', () => {
-    // On macOS it's common to re-create a window in the app when the
-    // dock icon is clicked and there are no other windows open.
-    if (applicationRef === null) {
-        createWindow();
-    }
-});
